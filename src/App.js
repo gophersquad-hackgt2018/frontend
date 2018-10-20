e400e45ef1a91c81e74ed6e31a8668758b88a033
Daniel Hsing <hsing.daniel@gmail.com>
2018-10-20 02:51:02 -0400

    feat: style index page
    Signed-off-by: Daniel Hsing <hsing.daniel@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,29 +1,41 @@
 import React, { Component } from "react";
 import Button from "@material-ui/core/Button";
+import Paper from "@material-ui/core/Paper";
 import { withStyles } from "@material-ui/core/styles";
 import withRoot from "./withRoot";
 import PhotoCameraIcon from "@material-ui/icons/PhotoCamera";
 import CloudIcon from "@material-ui/icons/CloudUpload";
 
-const styles = {
+const styles = theme => ({
     root: {
-        display: "flex",
-        justifyContent: "center"
+        width: "auto",
+        display: "block", // Fix IE 11 issue.
+        marginLeft: theme.spacing.unit * 3,
+        marginRight: theme.spacing.unit * 3,
+        [theme.breakpoints.up(600 + theme.spacing.unit * 3 * 2)]: {
+            width: 600,
+            marginLeft: "auto",
+            marginRight: "auto"
+        }
     },
     buttonContainer: {
         display: "flex",
-        flexDirection: "column"
+        flexDirection: "column",
+        marginTop: theme.spacing.unit * 8,
+        alignItems: "center",
+        padding: `${theme.spacing.unit * 2}px ${theme.spacing.unit *
+            3}px ${theme.spacing.unit * 3}px`
     }
-};
+});
 
 class App extends Component {
     render() {
-        const classes = this.props;
+        const { classes } = this.props;
         console.log(classes);
 
         return (
-            <div style={styles.root}>
-                <div style={styles.buttonContainer}>
+            <div className={classes.root}>
+                <Paper className={classes.buttonContainer}>
                     <Button color="primary">
                         <PhotoCameraIcon />
                         Take Photo
@@ -32,7 +44,7 @@ class App extends Component {
                         <CloudIcon />
                         Upload file
                     </Button>
-                </div>
+                </Paper>
             </div>
         );
     }
